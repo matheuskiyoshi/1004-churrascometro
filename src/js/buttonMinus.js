1f26ebbd20d403a927de8fef5a38033d5e73115c
matheuskiyoshi <matheuskiyoshi@gmail.com>
2023-09-24 21:20:12 -0300

    feat: botões de + e - da caixinha Mulher e caixinha Criança funcionando para os devidos inputs

--- src/js/buttonMinus.js
+++ src/js/buttonMinus.js
@@ -1,18 +1,55 @@
 const events = {
-    DECREMENTAR : "DECREMENTAR_EVENTO",
+    DECREMENTAR_HOMEM: "DECREMENTAR_HOMEM_EVENTO",
+    DECREMENTAR_MULHER: "DECREMENTAR_MULHER_EVENTO",
+    DECREMENTAR_CRIANCA: "DECREMENTAR_CRIANCA_EVENTO",
 }
 
-const buttonPlus = document.getElementsByClassName("input-group__button--small")[0];
 const calculator = document.getElementsByClassName("calculator")[0];
 
-buttonPlus.addEventListener("click", ()=>{
-    console.log("DISPARADO: " + events.DECREMENTAR); 
-    buttonPlus.dispatchEvent(new CustomEvent(events.DECREMENTAR, {bubbles: true}))
-})
+// DECREMENTO HOMEM
+const buttonMinusHomem = document.getElementsByClassName("input-group__button--small")[0];
 
-calculator.addEventListener(events.DECREMENTAR, () => {
+buttonMinusHomem.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.DECREMENTAR_HOMEM);
+    buttonMinusHomem.dispatchEvent(new CustomEvent(events.DECREMENTAR_HOMEM, { bubbles: true }));
+});
 
-    document.getElementById("homem").value--;
-    
-    console.log("RECEBIDO: " + events.DECREMENTAR);
+calculator.addEventListener(events.DECREMENTAR_HOMEM, () => {
+    const homemInput = document.getElementById("homem");
+    if (homemInput.value > 0) {
+        homemInput.value--;
+    }
+    console.log("RECEBIDO: " + events.DECREMENTAR_HOMEM);
+});
+
+// DECREMENTO MULHER
+const buttonMinusMulher = document.getElementsByClassName("input-group__button--small")[2];
+
+buttonMinusMulher.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.DECREMENTAR_MULHER);
+    buttonMinusMulher.dispatchEvent(new CustomEvent(events.DECREMENTAR_MULHER, { bubbles: true }));
+});
+
+calculator.addEventListener(events.DECREMENTAR_MULHER, () => {
+    const mulherInput = document.getElementById("mulher");
+    if (mulherInput.value > 0) {
+        mulherInput.value--;
+    }
+    console.log("RECEBIDO: " + events.DECREMENTAR_MULHER);
+});
+
+// DECREMENTO CRIANCA
+const buttonMinusCrianca = document.getElementsByClassName("input-group__button--small")[4];
+
+buttonMinusCrianca.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.DECREMENTAR_CRIANCA);
+    buttonMinusCrianca.dispatchEvent(new CustomEvent(events.DECREMENTAR_CRIANCA, { bubbles: true }));
+});
+
+calculator.addEventListener(events.DECREMENTAR_CRIANCA, () => {
+    const criancaInput = document.getElementById("crianca");
+    if (criancaInput.value > 0) {
+        criancaInput.value--;
+    }
+    console.log("RECEBIDO: " + events.DECREMENTAR_CRIANCA);
 });
--- src/js/buttonPlus.js
+++ src/js/buttonPlus.js
@@ -1,18 +1,46 @@
 const events = {
-    INCREMENTAR : "INCREMENTAR_EVENTO",
+    INCREMENTAR_HOMEM: "INCREMENTAR_HOMEM_EVENTO",
+    INCREMENTAR_MULHER: "INCREMENTAR_MULHER_EVENTO",
+    INCREMENTAR_CRIANCA: "INCREMENTAR_CRIANCA_EVENTO",
 }
 
-const buttonPlus = document.getElementsByClassName("input-group__button--small")[1];
 const calculator = document.getElementsByClassName("calculator")[0];
 
-buttonPlus.addEventListener("click", ()=>{
-    console.log("DISPARADO: " + events.INCREMENTAR); 
-    buttonPlus.dispatchEvent(new CustomEvent(events.INCREMENTAR, {bubbles: true}))
-})
+// INCREMENTO HOMEM
+const buttonPlusHomem = document.getElementsByClassName("input-group__button--small")[1];
 
-calculator.addEventListener(events.INCREMENTAR, () => {
+buttonPlusHomem.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.INCREMENTAR_HOMEM);
+    buttonPlusHomem.dispatchEvent(new CustomEvent(events.INCREMENTAR_HOMEM, { bubbles: true }));
+});
 
+calculator.addEventListener(events.INCREMENTAR_HOMEM, () => {
     document.getElementById("homem").value++;
-    
-    console.log("RECEBIDO: " + events.INCREMENTAR);
+    console.log("RECEBIDO: " + events.INCREMENTAR_HOMEM);
+});
+
+// INCREMENTO MULHER
+const buttonPlusMulher = document.getElementsByClassName("input-group__button--small")[3];
+
+buttonPlusMulher.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.INCREMENTAR_MULHER);
+    buttonPlusMulher.dispatchEvent(new CustomEvent(events.INCREMENTAR_MULHER, { bubbles: true }));
+});
+
+calculator.addEventListener(events.INCREMENTAR_MULHER, () => {
+    document.getElementById("mulher").value++;
+    console.log("RECEBIDO: " + events.INCREMENTAR_MULHER);
+});
+
+// INCREMENTO CRIANCA
+const buttonPlusCrianca = document.getElementsByClassName("input-group__button--small")[5];
+
+buttonPlusCrianca.addEventListener("click", () => {
+    console.log("DISPARADO: " + events.INCREMENTAR_CRIANCA);
+    buttonPlusCrianca.dispatchEvent(new CustomEvent(events.INCREMENTAR_CRIANCA, { bubbles: true }));
+});
+
+calculator.addEventListener(events.INCREMENTAR_CRIANCA, () => {
+    document.getElementById("crianca").value++;
+    console.log("RECEBIDO: " + events.INCREMENTAR_CRIANCA);
 });
